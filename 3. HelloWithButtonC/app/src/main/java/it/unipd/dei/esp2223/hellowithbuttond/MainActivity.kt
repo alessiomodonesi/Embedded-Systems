396bc4b3bd87760d050620a9cc30ba0fcbff15c0
alessiomodonesi <alessiomodo2004@gmail.com>
2026-03-25 13:19:23 +0100

    plus one with compose

--- 3. HelloWithButtonC/app/src/main/java/it/unipd/dei/esp2223/hellowithbuttond/MainActivity.kt
+++ 3. HelloWithButtonC/app/src/main/java/it/unipd/dei/esp2223/hellowithbuttond/MainActivity.kt
@@ -47,35 +47,64 @@ fun MainScreen(modifier: Modifier = Modifier)
 {
     val pleasePress = stringResource(R.string.please_press) // recupero la stringa dal file strings.xml
     val goodJob = stringResource(R.string.good_job)
+    val counter = stringResource(R.string.counter) // init della tv al valore iniziale
 
     // Reference: https://developer.android.com/jetpack/compose/state
-    var t by remember { mutableStateOf(pleasePress) } // il contenuto della casella di testo è segnalata come stato
+    var t1 by remember { mutableStateOf(pleasePress) } // il contenuto della casella di testo è segnalata come stato
+    var t2 by remember { mutableStateOf(counter) } // counter come stato
     // quando la casella di testo cambia, compose scatena la ricomposizione
 
     // Reference: https://developer.android.com/develop/ui/compose/layouts/constraintlayout
     ConstraintLayout(modifier = modifier) { // interfaccia utente
-        val (bu, tv) = createRefs() // creare le reference <=> creare gli ID nella classe View
+        val (bu1, bu2, tv1, tv2) = createRefs() // creare le reference <=> creare gli ID nella classe View
 
-        // bottone con testo
+        // bottone "Press me"
         Button(
-            modifier = Modifier.constrainAs(bu) { // modifier per il positioning
+            modifier = Modifier.constrainAs(bu1) { // modifier per il positioning
                 top.linkTo(parent.top)
                 start.linkTo(parent.start, margin = 4.dp)
             },
             // Set the action to be performed when the button is pressed
-            onClick = { t = goodJob } // Listener come funzione lambda
+            onClick = { t1 = goodJob } // Listener come funzione lambda
         ) {
             Text(text = stringResource(R.string.press_me)) // altro oggetto Text contenuto nel bottone
             // stringa costante che non fa parte dello stato
         }
 
+        // bottone "+1"
+        Button(
+            modifier = Modifier.constrainAs(bu2) {
+                top.linkTo(bu1.bottom)
+                start.linkTo(parent.start, margin = 4.dp)
+            },
+            // Set the action to be performed when the button is pressed
+            onClick = {
+                val i : Int = try {
+                    t2.toInt()
+                } catch (e: NumberFormatException) {
+                    -1
+                }
+                t2 = "${i + 1}"
+            }
+        ) {
+            Text(text = stringResource(R.string.plus_one))
+        }
+
         // casella di testo != text view
         Text(
-            modifier = Modifier.constrainAs(tv) { // modifier per il positioning
-                start.linkTo(bu.end, margin = 4.dp) // 4.dp == 4 pixel density independent
-                baseline.linkTo(bu.baseline)
+            modifier = Modifier.constrainAs(tv1) { // modifier per il positioning
+                start.linkTo(bu1.end, margin = 4.dp) // 4.dp == 4 pixel density independent
+                baseline.linkTo(bu1.baseline)
             },
-            text = t // il testo dell'oggetto è lo stato
+            text = t1 // il testo dell'oggetto è lo stato
+        )
+
+        Text(
+            modifier = Modifier.constrainAs(tv2) {
+                start.linkTo(bu2.end, margin = 4.dp)
+                baseline.linkTo(bu2.baseline)
+            },
+            text = t2 // il testo dell'oggetto è lo stato
         )
     }
 }
