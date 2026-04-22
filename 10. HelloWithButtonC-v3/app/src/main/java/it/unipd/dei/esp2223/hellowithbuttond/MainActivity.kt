cdde9ae811a5d66fa5a9117b038a0f3cb895426e
alessiomodonesi <alessiomodo2004@gmail.com>
2026-04-22 22:27:46 +0200

    sistemata repo

---
+++ 10. HelloWithButtonC-v3/app/src/main/java/it/unipd/dei/esp2223/hellowithbuttond/MainActivity.kt
@@ -0,0 +1,102 @@
+package it.unipd.dei.esp2223.hellowithbuttond
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import it.unipd.dei.esp2223.hellowithbuttond.ui.theme.ComposeHelloWithButtonTheme
+
+class MainActivity: ComponentActivity()
+{
+    // Called when the activity is first created
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge display on API level < 35
+        enableEdgeToEdge()
+
+        // Set and display the UI content
+        setContent {
+            ComposeHelloWithButtonTheme {
+                // Reference: https://developer.android.com/develop/ui/compose/components/scaffold
+                // The scaffold fills the whole display area
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // MainScreen consumes the insets
+                    // to keep the app UI away from the system UI and display cutouts
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier)
+{
+    val pleasePress = stringResource(R.string.please_press)
+    val goodJob = stringResource(R.string.good_job)
+
+    // Reference: https://developer.android.com/jetpack/compose/state
+    var t by rememberSaveable { mutableStateOf(pleasePress) }
+
+    // Reference: https://developer.android.com/develop/ui/compose/layouts/constraintlayout
+    ConstraintLayout(modifier = modifier) {
+        val (bu, bu2, tv) = createRefs()
+
+        Button(
+            modifier = Modifier.constrainAs(bu) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start, margin = 4.dp)
+            },
+            // Set the action to be performed when the button is pressed
+            onClick = { t = goodJob }
+        ) {
+            Text(text = stringResource(R.string.press_me))
+        }
+
+        Button(
+            modifier = Modifier.constrainAs(bu2) {
+                top.linkTo(bu.bottom)
+                start.linkTo(parent.start, margin = 4.dp)
+            },
+            // Set the action to be performed when the button is pressed
+            onClick = {
+                val i: Int = try { t.toInt() }
+                             catch(exception: NumberFormatException) { -1 }
+                // getString() not available,
+                // stringResource() can be invoked only in a composable function
+                t = "${i+1}"
+            }
+        ) {
+            Text(text = stringResource(R.string.plus_one))
+        }
+
+        Text(
+            modifier = Modifier.constrainAs(tv) {
+                start.linkTo(bu.end, margin = 4.dp)
+                baseline.linkTo(bu.baseline)
+            },
+            text = t
+        )
+    }
+}
+
+// Reference: https://developer.android.com/develop/ui/compose/tooling/previews
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
